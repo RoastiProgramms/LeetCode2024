33d5b018bcf859eefc7395c796ee07a595e956ad
RoastiProgramms <rostislavbankov@gmail.com>
2024-06-07 13:38:07 +0300

    June7

---
+++ src/com/company/June7.java
@@ -0,0 +1,38 @@
+package com.company;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class June7 {
+    public String replaceWords(List<String> dictionary, String sentence) {
+        HashSet<String> dict=new HashSet<>();
+        for(String word : dictionary)
+        {
+            dict.add(word);
+        }
+        String[] sent=sentence.split(" ");
+        for(int i=0;i<sent.length;i++)
+        {
+            for(String word : dict)
+            {
+                if(sent[i].startsWith(word))
+                {
+                    sent[i]=word;
+                }
+            }
+        }
+        StringBuilder sb= new StringBuilder();
+        for(int i=0;i<sent.length;i++)
+        {
+
+            sb.append(sent[i]);
+            if(i==sent.length-1)
+            {
+                break;
+            }
+            sb.append(" ");
+        }
+        return sb.toString();
+
+    }
+}
