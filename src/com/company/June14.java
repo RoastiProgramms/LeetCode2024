d80e9ff0eb6bfbd57be5c9487582be8733deeedc
RoastiProgramms <rostislavbankov@gmail.com>
2024-06-14 17:27:18 +0300

    June14

---
+++ src/com/company/June14.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class June14 {
+    public int minIncrementForUnique(int[] nums) {
+        Arrays.sort(nums);
+        int res=0;
+        for(int i=1;i<nums.length;i++)
+        {
+            if(nums[i]<=nums[i-1])
+            {
+                res+=nums[i-1]-nums[i]+1;
+                nums[i]=nums[i-1]+1;
+            }
+        }
+        return res;
+    }
+}
