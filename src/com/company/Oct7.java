2d1ce7456793b09b478f3ca041080e842f570c5f
RoastiProgramms <rostislavbankov@gmail.com>
2024-11-02 02:41:42 +0200

    Oct7

---
+++ src/com/company/Oct7.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.util.Stack;
+
+public class Oct7 {
+    public int minLength(String s) {
+        Stack<Character> stack=new Stack();
+        for(int i=0;i<s.length();i++)
+        {
+            if(stack.isEmpty())
+            {
+                stack.push(s.charAt(i));
+                continue;
+            }
+            if(s.charAt(i)=='B'&&stack.peek()=='A')
+            {
+                stack.pop();
+            }
+            else if(s.charAt(i)=='D'&&stack.peek()=='C')
+            {
+                stack.pop();
+            }
+            else
+            {
+                stack.push(s.charAt(i));
+            }
+        }
+        return stack.size();
+    }
+}
